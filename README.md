c28691bb5adb322cd466df4d2d60d55e442b11af
Jacob Paton <jacobpaton@shaw.ca>
2022-01-15 14:49:18 -0700

    Init commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Chip8 Emu
+
+This project is a chip8 emulator written for the UAlberta CompE Club's HackED 2022 hackathon.
+
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+int main(int argc, char* argv[]) {
+    if (argc != 4) {
+        std::cout << "Usage: ./chip8 scale delay romFile" << std::endl;
+    }
+
+    return 0;
+}
